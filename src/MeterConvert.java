ede8ff3f2e3f7a25dc5b9c7b92796f88dd0e4a0d
samiaasad <samiaasad604@gmail.com>
2024-03-16 20:58:06 +0600

    E4.5

---
+++ src/MeterConvert.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class MeterConvert {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a distance in meters: ");
+        double meters = scanner.nextDouble();
+        double miles = meters * 0.000621371;
+        double feet = meters * 3.28084;
+        double inches = meters * 39.37008;
+        System.out.printf("%.2f meters is equal to:\n", meters);
+        System.out.printf("%.2f miles\n", miles);
+        System.out.printf("%.2f feet\n", feet);
+        System.out.printf("%.2f inches\n", inches);
+    }
+}
